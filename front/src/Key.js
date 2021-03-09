f790c87fdff6373aeec1dc0504de6ed54701d8a9
gomjellie <gomjellie@gmail.com>
2021-03-09 10:49:41 +0900

    상태관리를 위해 Context 도입 (#15)

--- front/src/Key.js
+++ front/src/Key.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import {KeyboardContext} from './Contexts';
+import MacKeyboardStyles from './MacKeyboard.styles';
 
-function Key({ value, customStyle }) {
+function Key({ displayChar, code }) {
+  const keyboard = useContext(KeyboardContext);
+  
+  const hitCheck = useCallback((code) => {
+    // console.log(keyboard);
+    return keyboard.currentKey === code ? MacKeyboardStyles.KeyPressedStyle : {};
+  }, [keyboard]);
+
   return (
-    <div className="key noselect" style={{ ...KeyStyle, ...customStyle }}>
-      {value}
+    <div className="key noselect" style={{ ...KeyStyle, ...styleFactory(code), ...hitCheck(code) }}>
+      {displayChar}
     </div>
   )
+}
+
+function styleFactory(value) {
+  let retStyle = MacKeyboardStyles.KeyDefaultStyle;
+  styleFactory.styleMap = {
+    'fn': MacKeyboardStyles.KeySmallStyle,
+    'ControlLeft': MacKeyboardStyles.KeySmallStyle,
+    'ControlRight': MacKeyboardStyles.KeySmallStyle,
+    'AltLeft': MacKeyboardStyles.KeySmallStyle,
+    'AltRight': MacKeyboardStyles.KeySmallStyle,
+    'Tab': MacKeyboardStyles.KeyEscStyle,
+    'Backspace': MacKeyboardStyles.KeyEscStyle,
+    'Capslock': MacKeyboardStyles.KeyCapsLockStyle,
+    'Space': MacKeyboardStyles.KeySpaceBarStyle,
+    'MetaLeft': MacKeyboardStyles.KeyCommandStyle,
+    'MetaRight': MacKeyboardStyles.KeyCommandStyle,
+    'Enter': MacKeyboardStyles.KeyReturnStyle,
+    'ShiftLeft': MacKeyboardStyles.KeyShiftStyle,
+    'ShiftRight': MacKeyboardStyles.KeyShiftStyle,
+  }
+
+  retStyle = { ...retStyle, ...styleFactory.styleMap[value] };
+
+  return retStyle;
 }
 
 const KeyStyle = {
@@ -19,8 +52,8 @@ const KeyStyle = {
 };
 
 Key.propTypes = {
-  value: PropTypes.string,
-  customStyle: PropTypes.object,
+  displayChar: PropTypes.string,
+  code: PropTypes.string,
 };
 
 export default React.memo(Key);
