dbddfba5907044eb6cc294fbae92075db0f28fa9
gomjellie <gomjellie@gmail.com>
2021-03-09 10:51:04 +0900

    key, keyboard 스타일 나눔

--- front/src/Key.js
+++ front/src/Key.js
@@ -1,55 +1,46 @@
 import React, { useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {KeyboardContext} from './Contexts';
-import MacKeyboardStyles from './MacKeyboard.styles';
+import KeyStyles from './Key.styles';
 
 function Key({ displayChar, code }) {
   const keyboard = useContext(KeyboardContext);
   
   const hitCheck = useCallback((code) => {
     // console.log(keyboard);
-    return keyboard.currentKey === code ? MacKeyboardStyles.KeyPressedStyle : {};
+    return keyboard.currentKey === code ? KeyStyles.KeyPressedStyle : {};
   }, [keyboard]);
 
   return (
-    <div className="key noselect" style={{ ...KeyStyle, ...styleFactory(code), ...hitCheck(code) }}>
+    <div className="key noselect" style={{ ...styleFactory(code), ...hitCheck(code) }}>
       {displayChar}
     </div>
   )
 }
 
 function styleFactory(value) {
-  let retStyle = MacKeyboardStyles.KeyDefaultStyle;
+  let retStyle = KeyStyles.KeyDefaultStyle;
   styleFactory.styleMap = {
-    'fn': MacKeyboardStyles.KeySmallStyle,
-    'ControlLeft': MacKeyboardStyles.KeySmallStyle,
-    'ControlRight': MacKeyboardStyles.KeySmallStyle,
-    'AltLeft': MacKeyboardStyles.KeySmallStyle,
-    'AltRight': MacKeyboardStyles.KeySmallStyle,
-    'Tab': MacKeyboardStyles.KeyEscStyle,
-    'Backspace': MacKeyboardStyles.KeyEscStyle,
-    'Capslock': MacKeyboardStyles.KeyCapsLockStyle,
-    'Space': MacKeyboardStyles.KeySpaceBarStyle,
-    'MetaLeft': MacKeyboardStyles.KeyCommandStyle,
-    'MetaRight': MacKeyboardStyles.KeyCommandStyle,
-    'Enter': MacKeyboardStyles.KeyReturnStyle,
-    'ShiftLeft': MacKeyboardStyles.KeyShiftStyle,
-    'ShiftRight': MacKeyboardStyles.KeyShiftStyle,
+    'fn': KeyStyles.KeySmallStyle,
+    'ControlLeft': KeyStyles.KeySmallStyle,
+    'ControlRight': KeyStyles.KeySmallStyle,
+    'AltLeft': KeyStyles.KeySmallStyle,
+    'AltRight': KeyStyles.KeySmallStyle,
+    'Tab': KeyStyles.KeyEscStyle,
+    'Backspace': KeyStyles.KeyEscStyle,
+    'Capslock': KeyStyles.KeyCapsLockStyle,
+    'Space': KeyStyles.KeySpaceBarStyle,
+    'MetaLeft': KeyStyles.KeyCommandStyle,
+    'MetaRight': KeyStyles.KeyCommandStyle,
+    'Enter': KeyStyles.KeyReturnStyle,
+    'ShiftLeft': KeyStyles.KeyShiftStyle,
+    'ShiftRight': KeyStyles.KeyShiftStyle,
   }
 
   retStyle = { ...retStyle, ...styleFactory.styleMap[value] };
 
   return retStyle;
 }
-
-const KeyStyle = {
-  padding: '10px',
-  paddingTop: '10px',
-  color: 'white',
-  margin: '4px',
-  fontSize: '1.5rem',
-  boxShadow: '0 1px 2px 0 #12CFC0, 0 1px 3px 1px #bdeaff'
-};
 
 Key.propTypes = {
   displayChar: PropTypes.string,
--- front/src/MacKeyboard.styles.js
+++ front/src/MacKeyboard.styles.js
@@ -1,53 +1,10 @@
 
-const KeyDefaultStyle = {
-  color: "#FB9FB1", // F5F5F5
-  width: '32px',
-};
-
-const KeyPressedStyle = {
-  backgroundColor: '#505050', // bright black
-  color: '#F5F5F5',
-};
-
-const KeyEscStyle = {
-  width: '60px',
-  fontSize: '1.2em',
-};
-
-const KeyCapsLockStyle = {
-  width: '70px',
-  fontSize: '1.2em',
-};
-
-const KeyReturnStyle = {
-  width: '70px',
-  fontSize: '1.2em',
-};
-
-const KeyShiftStyle = {
-  width: '80px',
-};
-
-const KeySpaceBarStyle = {
-  width: '300px',
-};
-
-const KeyCommandStyle = {
-  width: '45px',
-};
-
-const KeySmallStyle = {
-  width: '30px',
-};
-
 const KeyboardRowStyle = {
   display: 'flex',
 };
 
-const KeyStyles = {
-  KeyDefaultStyle, KeyPressedStyle, KeyEscStyle, KeyCapsLockStyle,
-  KeyReturnStyle, KeyShiftStyle, KeySpaceBarStyle, KeyCommandStyle, KeySmallStyle,
+const KeyboardStyles = {
   KeyboardRowStyle,
 };
 
-export default KeyStyles;
+export default KeyboardStyles;
