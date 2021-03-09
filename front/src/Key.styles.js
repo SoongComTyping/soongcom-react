1560d8bc608426710c0ef7d0e08d2ee2855e32bd
gomjellie <gomjellie@gmail.com>
2021-03-09 10:52:19 +0900

    key, keyboard 스타일 나눔 (#16)

---
+++ front/src/Key.styles.js
@@ -0,0 +1,53 @@
+
+const KeyDefaultStyle = {
+  padding: '10px',
+  paddingTop: '10px',
+  color: "#FB9FB1", // F5F5F5
+  width: '32px',
+  margin: '4px',
+  fontSize: '1.5rem',
+  boxShadow: '0 1px 2px 0 #12CFC0, 0 1px 3px 1px #bdeaff'
+};
+
+const KeyPressedStyle = {
+  backgroundColor: '#505050', // bright black
+  color: '#F5F5F5',
+};
+
+const KeyEscStyle = {
+  width: '60px',
+  fontSize: '1.2em',
+};
+
+const KeyCapsLockStyle = {
+  width: '70px',
+  fontSize: '1.2em',
+};
+
+const KeyReturnStyle = {
+  width: '70px',
+  fontSize: '1.2em',
+};
+
+const KeyShiftStyle = {
+  width: '80px',
+};
+
+const KeySpaceBarStyle = {
+  width: '300px',
+};
+
+const KeyCommandStyle = {
+  width: '45px',
+};
+
+const KeySmallStyle = {
+  width: '30px',
+};
+
+const KeyStyles = {
+  KeyDefaultStyle, KeyPressedStyle, KeyEscStyle, KeyCapsLockStyle,
+  KeyReturnStyle, KeyShiftStyle, KeySpaceBarStyle, KeyCommandStyle, KeySmallStyle,
+};
+
+export default KeyStyles;
