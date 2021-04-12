736ee4f14108ad8a96b54262f10c5dbb4f9b0529
Donggyu Kim <ptsaturn68@daum.net>
2021-04-13 02:06:46 +0900

    엔터 입력시 다음 문장 세팅 및 입력 초기화 구현

--- front/src/features/practices/sentences/PracticeSentenceTask.js
+++ front/src/features/practices/sentences/PracticeSentenceTask.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, useRef } from 'react';
 import Sentence from './Sentence';
 import useSound from 'use-sound';
 import keySoundAsset from '../../../mechanicalKeyboard.mp3';
 import { KoreanInputMethod, inko} from '../../helpers/KoreanHelper';
 
-
 function PracticeSentenceTask () {
   const tempData = ['숭실대학교 컴퓨터학부가 생겨났다.',
     '모든게 여전한 나라에서, 다른 느낌을 받는다.', '아이즈원의 노래는 잘 맞추는 사람이 있다.', 
     '여러 곳을 들려 선물을 준비한 보람이 있다.', '토파즈 보석을 캤던 곳에서 다이아몬드를 주울 확률은 얼마나 될지 모르겠다'];
-  const currentResult = '소 가는데 말도 간다.';
   const finishedInput =' ';
   const finishedResult='';
 
   const [language] = useState("korean");
-  //const [currentKey, setCurrentKey] = useState("");
+  // const [currentKey, setCurrentKey] = useState("");
   const [userInput, setUserInput] = useState("");
   const [koreanBuffer, setKoreanBuffer] = useState("");
+
+  var step = useRef(0);
+  const [currentResult, setCurrentResult] = useState("");
+  const [currentInput, setCurrentInput] = useState("");
 
   const [playKeyPress] = useSound(
     keySoundAsset,
@@ -25,29 +27,38 @@ function PracticeSentenceTask () {
   )
 
   const onKeyDown = useCallback((event) => {
-    //setCurrentKey(event.code);
+    // setCurrentKey(event.code);
     playKeyPress();
+   
     if (language === 'korean') {
       setKoreanBuffer((buf) => {
         const { nextUserInput, nextBuf } = KoreanInputMethod(buf, event, userInput);
         if (nextUserInput !== userInput) {
           setUserInput(nextUserInput);
+          if(event.code === 'Enter') {
+            // console.log("hi");
+            // console.log(step.current);
+            step.current = step.current+0.5; // 왜 2배로 증가하는지 모르겠음.
+            setCurrentResult(tempData[step.current]);
+            setUserInput("");
+            setCurrentInput("");
+          }
         }
         return nextBuf;
       });
       return;
     }
 
-    
-
     setUserInput((body) => {
+      console.log("enter");
       if (event.key === 'Backspace') {
         event.preventDefault(); // for firefox browser
         return body.slice(0, -1);
       }
 
-      if (event.key === 'Enter')
+      if (event.key === 'Enter') {
         return body.concat('\n');
+      }
 
       if (event.key.length > 1)
         return body;
@@ -57,12 +68,20 @@ function PracticeSentenceTask () {
   }, [playKeyPress, language, userInput])
 
   const onKeyUp = useCallback(() => {
-    //setCurrentKey("");
+    // setCurrentKey("");
   }, [])
 
   useEffect(() => {
-    document.body.addEventListener("keydown", onKeyDown);
+    setCurrentResult(tempData[step.current]);
+  }, [])
 
+  useEffect(() => {
+    // console.log(userInput);
+    setCurrentInput(userInput+inko.en2ko(koreanBuffer));
+  });
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", onKeyDown);
     return () => {
       document.body.removeEventListener("keydown", onKeyDown);
     }
@@ -84,7 +103,7 @@ function PracticeSentenceTask () {
       <Sentence type = 'finished-result' sentence = {finishedResult}/>
       <Sentence type = 'finished-input' sentence = {finishedInput}/>
       <Sentence type = 'current-result' sentence = {currentResult}/>
-      <Sentence type = 'current-input' sentence = {userInput+inko.en2ko(koreanBuffer)}/>
+      <Sentence type = 'current-input' sentence = {currentInput}/>
       {sentences}
     </div>
   );
