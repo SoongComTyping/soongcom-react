acead14f28d511212bacfc085fe8fe582cbe3fdc
yegyeom <reallyy1111@gmail.com>
2021-06-21 19:32:35 +0900

    스크립트 보기 구조 구현

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Header from './components/Header';
 import AddScript from "./features/scripts/AddScript";
+import ShowScript from "./features/scripts/ShowScript";
 import ParagraphPractice from "./features/practices/paragraphs";
 import WordPractice from "./features/practices/words";
 import KeyPractice from "./features/practices/keys";
@@ -15,6 +16,7 @@ function App() {
           <Header />
           <Switch>
             <Route path="/add-script" component={AddScript} />
+            <Route path="/show-script" component={ShowScript} />
             <Route exact path="/" component={ParagraphPractice} />
             <Route exact path="/practice-key" component={KeyPractice} />
             <Route exact path="/practice-word" component={WordPractice} />
---
+++ front/src/features/scripts/ShowScript.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React from 'react';
+import Title from '../../components/Title';
+import {useEffect} from "react"
+import '../../sass/main.css';
+
+function ShowScript() {
+
+  async function getList() {
+    axios({
+      method: 'get',
+      url: 'http://soongcom.kro.kr:3001/practice/list',
+      responseType: 'stream'
+    })
+      .then(function () {
+      });
+  }
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  return (
+    <div>
+      <Title title = '스크립트 연습'/>
+      <form className="ListForm">
+        <div className="TabSetting">
+          <div className="NavyTab">
+            <button className="NavyButton" type="button">탐색</button>
+            <div className="Line"></div>
+            <button className="NavyButton" type="button">인기</button>
+          </div>
+          <input className="SearchTab"
+            type="text" 
+            placeholder="스크립트를 찾아보세요!">
+          </input>
+        </div>
+        <div className="ListBody">
+
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ShowScript;
