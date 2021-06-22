271f121ee1950f0ada27b014f4e406c276cf2c9f
yegyeom <reallyy1111@gmail.com>
2021-06-23 00:30:27 +0900

    메인 화면 이미지 삽입

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Header from './components/Header';
 import AddScript from "./features/scripts/AddScript";
 import ShowScript from "./features/scripts/ShowScript";
-import ParagraphPractice from "./features/practices/paragraphs";
+import SimpleSlider from "./features/practices/paragraphs";
 import WordPractice from "./features/practices/words";
 import KeyPractice from "./features/practices/keys";
 import PracticeSentence from "./features/practices/sentences/PracticeSentence"
@@ -18,7 +18,7 @@ function App() {
           <Switch>
             <Route path="/add-script" component={AddScript} />
             <Route path="/show-script" component={ShowScript} />
-            <Route exact path="/" component={ParagraphPractice} />
+            <Route exact path="/" component={SimpleSlider} />
             <Route exact path="/practice-key" component={KeyPractice} />
             <Route exact path="/practice-word" component={WordPractice} />
             <Route path="/practice-sentence" component={PracticeSentence} />
--- front/src/features/practices/paragraphs/index.js
+++ front/src/features/practices/paragraphs/index.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
+import '../../../sass/main.css';
+import main_image_2 from '../../../assets/main_image_1.jpg';
+import main_image_3 from '../../../assets/main_image_3.jpg';
+import main_image_1 from '../../../assets/main_image_5.jpg';
+import styled from 'styled-components';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-function ParagraphPractice() {
+const StyledSlider = styled(Slider)`
+    .slick-slide div{
+      outline: none;
+    }
+`;
 
-  return (
-    <div >
-      
-    </div>
-  );
+const ImageContainer = styled.div`
+  margin: 0px;
+`;
+
+const Image = styled.img`
+width:2000px;
+height:750px;
+object-fit: cover;
+`;
+
+const items = [
+  { id: 1, url: main_image_1 },
+  { id: 2, url: main_image_2 },
+  { id: 3, url: main_image_3 },
+];
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 4000, 
+      pauseOnHover: false, 
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <StyledSlider {...settings}>
+          {items.map(item => {
+            return (
+              <div key={item.id}>
+                <ImageContainer>
+                  <Image src={item.url} />
+                </ImageContainer>
+              </div>
+            );
+          })}
+        </StyledSlider>
+      </div>
+    );
+  }
 }
-
-export default ParagraphPractice;
